f9dc18d6bf47ac220ed25313729f824c618f966c
FleetAdmiralButter <ming.quah@protonmail.com>
2022-11-16 21:47:43 +1100

    Partial rewrite of updater to improve stability

--- src/IINACT/IINACTUpdateEndpoint.php
+++ src/IINACT/IINACTUpdateEndpoint.php
@@ -3,7 +3,7 @@
 namespace Drupal\xom_web_services\IINACT;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\State\StateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\Core\Cache\CacheableResponse;
@@ -14,40 +14,33 @@ use Drupal\Core\Cache\CacheableResponse;
  */
 class IINACTUpdateEndpoint extends ControllerBase {
 
-  private $cache;
-  public function __construct(CacheBackendInterface $cache) {
-    $this->cache = $cache;
+  private $state;
+  public function __construct(StateInterface $state) {
+    $this->state = $state;
   }
 
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('cache.default')
+      $container->get('state')
     );
   }
 
   public function serve() {
-    $version = $this->fetchFromCache('iinact_plugin_latest');
+    $version = $this->fetchFromState('xom_web_services.iinact_plugin_latest');
     $response = new CacheableResponse($version, 200);
     $response->getCacheableMetadata()->addCacheTags(['iinact_plugin_latest']);
     return $response;
   }
 
   public function serveDownload() {
-    $url = $this->fetchFromCache('iinact_plugin_latest_url');
-    return new TrustedRedirectResponse($url, 307);
+    $url = $this->fetchFromState('xom_web_services.iinact_plugin_download');
+    $response = new TrustedRedirectResponse($url, 307);
+    $response->getCacheableMetadata()->addCacheTags(['iinact_plugin_latest']);
+    return $response;
   }
 
-  private function fetchFromCache($cid) {
-    $result = $this->cache->get($cid, TRUE)->data;
-
-    /**
-     * Ensure the caching backend gave us a valid result.
-     */
-    if (empty($result)) {
-      \Drupal::service('xom_web_services.iinact_update_manager')->pluginGetLatest();
-      $result = $this->cache->get($cid, TRUE)->data;
-    }
-    return $result;
+  private function fetchFromState($sid) {
+    return $this->state->get($sid);
   }
 
 }
--- src/IINACT/IINACTUpdateManager.php
+++ src/IINACT/IINACTUpdateManager.php
@@ -2,18 +2,22 @@
 
 namespace Drupal\xom_web_services\IINACT;
 
-use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Site\Settings;
+use Drupal\Core\State\StateInterface;
 use GuzzleHttp\Client;
 use Drupal\Component\Utility\Xss;
 
 class IINACTUpdateManager {
 
-    private CacheBackendInterface $cache;
+    private StateInterface $state;
     private $http_client;
-    public function __construct(CacheBackendInterface $cache, Client $http_client) {
-        $this->cache = $cache;
+    private $webhook_url;
+    public function __construct(StateInterface $state, Client $http_client) {
+        $this->state = $state;
         $this->http_client = $http_client;
+        $this->webhook_url = Settings::get('xom_web_services.iinact_webhook_url');
     }
 
     public function pluginGetLatest() {
@@ -33,18 +37,47 @@ class IINACTUpdateManager {
     }
 
     private function updateCachedPluginVersion($version, $url) {
-      $old_version = $this->cache->get('iinact_plugin_latest', FALSE);
-      if (empty($old_version)) {
-        $old_version = '';
-      } else {
-        $old_version = $old_version->data;
-      }
+      $old_version = $this->state->get('xom_web_services.iinact_plugin_latest', '0.0.0.0');
       if ($old_version != $version) {
+        $this->state->set('xom_web_services.iinact_plugin_latest', $version);
+        $this->state->set('xom_web_services.iinact_plugin_download', $url);
         Cache::invalidateTags(['iinact_plugin_latest']);
-        $this->cache->set('iinact_plugin_latest_url', $url, CacheBackendInterface::CACHE_PERMANENT, ['iinact_plugin_latest']);
-        $this->cache->set('iinact_plugin_latest', $version, CacheBackendInterface::CACHE_PERMANENT, ['iinact_plugin_latest']);
+        $this->postDiscordMessage($version, $url);
       } else {
         \Drupal::logger('xom_web_services')->debug('IINACT: Cache sync skipped.');
       }
     }
+
+    private function postDiscordMessage($version, $url) {
+      $body = [
+        'content' => $this->templateMessage($version, $url),
+        'username' => 'www.iinact.com',
+        'avatar_url' => "https://content.xivmac.com/sites/default/files/2022-04/discord_bot.png",
+      ];
+      $body = Json::encode($body);
+      $request_params = [
+        'headers' => [
+          'Content-Type' => 'application/json'
+        ],
+        'body' => $body
+      ];
+      try {
+        $this->http_client->post($this->webhook_url, $request_params);
+      } catch (\Exception) {
+        \Drupal::service('messenger')->addError('Discord webhook error.');
+      }
+    }
+
+    private function templateMessage($version, $url) {
+      return <<<EOT
+      Found new FFXIV_ACT_Plugin release!
+      
+      Propagating to caches, allow +/- 5 minutes.
+      
+      Version: $version
+      Download URL: $url
+      EOT;
+  }
+
+
 }
